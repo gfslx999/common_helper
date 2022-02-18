6b5356963fdd8dfdeb6588647c668cb3df987eb5
fengseng.gao <https://gitlab.hexcloud.cn/dashboard/projects>
2022-02-18 10:14:40 +0800

    添加-jitpack配置

--- app/src/main/java/com/fs/freedom/common_helper/MainActivity.kt
+++ app/src/main/java/com/fs/freedom/common_helper/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import com.fs.freedom.basic.helper.NetworkHelper
 import com.fs.freedom.basic.util.LogUtil
 import com.fs.freedom.basic.util.ToastUtil
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +28,63 @@ class MainActivity : AppCompatActivity() {
         btnSecond.setOnClickListener {
             ToastUtil.showToast(this,"我才是第一")
         }
+
+        val firstChildList = listOf(
+            ChildData(useTime = "1"),
+            ChildData(useTime = "2"),
+            ChildData(useTime = "2"),
+            ChildData(useTime = "1"),
+            ChildData(useTime = "2"),
+            ChildData(useTime = "1"),
+            ChildData(useTime = "1"),
+            ChildData(useTime = "2"),
+            ChildData(useTime = "3"),
+        )
+        val secondChildList = listOf(
+            ChildData(useTime = "2"),
+            ChildData(useTime = "2"),
+            ChildData(useTime = "2"),
+            ChildData(useTime = "2"),
+            ChildData(useTime = "2"),
+            ChildData(useTime = "1"),
+            ChildData(useTime = "1"),
+            ChildData(useTime = "2"),
+            ChildData(useTime = "1"),
+        )
+        val thirdChildList = listOf(
+            ChildData(useTime = "2"),
+            ChildData(useTime = "2"),
+            ChildData(useTime = "2"),
+        )
+        val originalList = listOf(
+            TestData(id = 1, name = "aa", childList = firstChildList),
+            TestData(id = 2, name = "bb", childList = secondChildList),
+            TestData(id = 3, name = "cc", childList = thirdChildList),
+        )
+
+        toPackage(originalList)
     }
+
+    private fun toPackage(originalList: List<TestData>?) {
+        if (originalList.isNullOrEmpty()) {
+            return
+        }
+        LogUtil.logI("originalList: ${Gson().toJson(originalList)}")
+        val firstList = mutableListOf<TestData>()
+        val secondList = mutableListOf<TestData>()
+        originalList.forEach { parentData ->
+            val useOneTimeList: List<ChildData> = parentData.childList.filter { it.useTime == "1" }
+            val newData = TestData(
+                id = parentData.id,
+                name = parentData.name,
+                childList = useOneTimeList
+            )
+            firstList.add(newData)
+            val useTwoTimeList: List<ChildData> = parentData.childList.filter { it.useTime == "2" }
+            secondList.add(newData.copyOf(newList = useTwoTimeList))
+        }
+        LogUtil.logI("firstList: ${Gson().toJson(firstList)}")
+        LogUtil.logI("secondList: ${Gson().toJson(secondList)}")
+    }
+
 }
--- basic/src/main/java/com/fs/freedom/basic/BasicInitial.kt
+++ basic/src/main/java/com/fs/freedom/basic/BasicInitial.kt
@@ -1,7 +1,5 @@
 package com.fs.freedom.basic
 
-import android.annotation.SuppressLint
-import android.content.Context
 import com.fs.freedom.basic.util.LogUtil
 
 class BasicInitial {
